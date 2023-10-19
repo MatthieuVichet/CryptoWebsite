557b92a81471f5d3ba5d97838d6de7c53c54e5e6
MatthieuVichet <matthieu.vichet@efrei.net>
2023-10-19 20:52:00 +0200

    BaseButton creation + init new components

--- cryptowebsite/src/views/HomeView.vue
+++ cryptowebsite/src/views/HomeView.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
        <h1>This is a home page</h1>
+       <Button title="Click me"/>
 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Button from '../components/BaseButton.vue';
 export default {
   name: 'HomeView',
-}
+  components: {
+    Button, 
+  },
+};
 </script>
